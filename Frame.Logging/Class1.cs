1850c1f7aca9de400d1de1328ac0562f751a43ec
martin0714 <lihanwork@163.com>
2015-09-19 00:06:55 +0800

    完善框架

---
+++ Frame.Logging/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frame.Logging
+{
+    public class Class1
+    {
+    }
+}
